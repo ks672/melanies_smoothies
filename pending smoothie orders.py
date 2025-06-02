36ae14a6604d2f0ed88ed5eb72d39b521860ab3a
Kamil Skibicki <47693397+ks672@users.noreply.github.com>
2025-06-02 21:37:58 +0100

    pending smoothie orders

---
+++ pending smoothie orders.py
@@ -0,0 +1,69 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
+# Write directly to the app
+st.title(f"Here are the new kitchen orders! :clipboard:")
+st.write(
+  """Click on the check mark to mark orders as complete
+  """
+)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+
+if my_dataframe:
+    editable_df = st.data_editor(my_dataframe, disabled=["INGREDIENTS", "NAME_ON_ORDER", "ORDER_UID", "ORDER_TS"], hide_index=True)
+    submit = st.button('Complete orders')
+    
+    if submit:
+#    update_stmt = """ update smoothies.public.orders where  = """' + """ 
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+
+        try:
+            og_dataset.merge(edited_dataset
+                             , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                             , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                            )
+            st.success('Order(s) Updated!', icon="✅")
+
+        except:
+            st.write('Something went wrong')
+
+else: 
+    st.success('No pending orders', icon="👍");
+
+
+
+st.write(
+  """All completed orders :white_check_mark:
+  """
+)
+
+completed_orders = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==1).collect()
+
+if completed_orders:
+    st.dataframe(data=completed_orders, use_container_width=True)
+
+else:
+    st.success('No completed orders to display')
+
+
+#if
+#update_stmt = """ update smoothies.public.orders where name_on_order = """' + '""" 
+
+#st.dataframe(data=edit_dataframe, use_container_width=True)
+
+
+
+
+#update_stmt = """ update smoothies.public.orders where name_on_order = """' + ''""" 
+
+
+#if time_to_insert:
+#            session.sql(my_insert_stmt).collect()
+#            st.success('Your Smoothie is ordered!', icon="✅")
+
+
