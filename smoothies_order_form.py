8e00492a41025aa8435372cde6cc5f26ff82e1fe
Kamil Skibicki <47693397+ks672@users.noreply.github.com>
2025-06-03 14:54:28 +0100

    Update smoothies_order_form.py
    api call

--- smoothies_order_form.py
+++ smoothies_order_form.py
@@ -52,6 +52,10 @@ if ingredients_list:
             st.success('Your Smoothie is ordered!', icon="✅")
 
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
 #option = st.selectbox(
 #    "Choose your fruit?",
 #    ('Banana', 'Strawberries', 'Berries'),
