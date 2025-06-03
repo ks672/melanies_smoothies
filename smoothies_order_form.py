1749602c78a3551b4e3d6e0270ca404fbe66e824
Kamil Skibicki <47693397+ks672@users.noreply.github.com>
2025-06-03 15:15:41 +0100

    Update smoothies_order_form.py

--- smoothies_order_form.py
+++ smoothies_order_form.py
@@ -55,7 +55,7 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-st_df = st.datafrane(data=smoothiefroot_response.json(), use_container_width=True)
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 #option = st.selectbox(
 #    "Choose your fruit?",
